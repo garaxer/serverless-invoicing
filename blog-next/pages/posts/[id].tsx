0f4855354964e8ccd378321b5595b0f68b0ea5f4
Gary Bagnall <gbagnall8@gmail.com>
2022-08-01 14:42:53 +1000

    Added new next app

---
+++ blog-next/pages/posts/[id].tsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from '../../components/date';
+
+export async function getStaticProps({ params }: any) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }: any) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
