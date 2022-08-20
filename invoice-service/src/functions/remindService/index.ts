e2d1d9689b2e498ddb08ccad6b3b1e555386df5f
Gary Bagnall <gbagnall8@gmail.com>
2022-08-20 16:53:37 +1000

    Correct ScheduleExpression

--- invoice-service/src/functions/remindService/index.ts
+++ invoice-service/src/functions/remindService/index.ts
@@ -2,5 +2,5 @@ import { handlerPath } from "@libs/handler-resolver";
 
 export default {
   handler: `${handlerPath(__dirname)}/remindService.main`,
-  events: [{ schedule: "rate(1 days)" }],
+  events: [{ schedule: "rate(1 day)" }],
 };
