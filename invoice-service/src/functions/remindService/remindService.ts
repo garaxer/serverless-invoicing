ddbd6c544f2483664c4de76806bc5d16ad71e60e
Gary Bagnall <gbagnall8@gmail.com>
2022-08-20 19:14:12 +1000

    Add logging

--- invoice-service/src/functions/remindService/remindService.ts
+++ invoice-service/src/functions/remindService/remindService.ts
@@ -10,7 +10,10 @@ const remindService = async (_event: APIGatewayEvent, _context: Context) => {
     const closePromises = currentOpenServices.map((service) =>
       serviceMailer(service)
     );
+    console.log({ closePromises });
     await Promise.all(closePromises);
+    console.log("Done");
+
     return { closed: closePromises.length };
   } catch (error) {
     console.error(error);
--- invoice-service/src/libs/booking/serviceMailer.ts
+++ invoice-service/src/libs/booking/serviceMailer.ts
@@ -22,18 +22,25 @@ export async function serviceMailer(service: Service, sendOverride = false) {
   const isAfter = new Date(startDateTime) > now;
   const isClosed = bookingStatus.includes("CLOSED");
 
-  // Has it been a bit since last reminder email and it is overdue and unpaid
-  // if (reminderSentDate && !sendOverride) {
-  //   const REMINDER_INTERVAL = 86400000 * 1; //1 day * 1
-  //   const lastSent = new Date(reminderSentDate);
-  //   const followingDay = new Date(lastSent.getTime() + REMINDER_INTERVAL); // + 1 day in ms
+  // Has it been a bit since last reminder email and it is not after service end and unpaid
+  if (reminderSentDate && !sendOverride) {
+    const REMINDER_INTERVAL = 86400000 * 1; //1 day * 1
+    const lastSent = new Date(reminderSentDate);
+    const followingDay = new Date(lastSent.getTime() + REMINDER_INTERVAL); // + 1 day in ms
 
-  //   console.log({ isAfter, isClosed, now, followingDay });
+    console.log({
+      isAfter,
+      isClosed,
+      now,
+      followingDay,
+      "now < followingDay": now < followingDay,
+    });
 
-  //   if (isAfter || isClosed || now < followingDay) {
-  //     return;
-  //   }
-  // }
+    if (!isAfter || isClosed || now < followingDay) {
+      console.log("already sent recently");
+      return;
+    }
+  }
 
   const params = {
     TableName: process.env.BOOKINGS_TABLE_NAME,
@@ -46,27 +53,28 @@ export async function serviceMailer(service: Service, sendOverride = false) {
 
   await dynamodb.update(params).promise();
 
-  console.log("Sending invoice");
+  console.log("Sending reminder");
 
   const notifyBookees = sendEmail
     ? timeSlots.flatMap((t) =>
-        t.attendees.map((a) =>
-          sqs
+        t.attendees.flatMap((a) => {
+          const params = {
+            subject: `${title.replace("{name}", a.name)}`,
+            recipient: a.email,
+            body: `s ${description?.replace(
+              "{startDateTime}",
+              new Date(startDateTime)
+                .toLocaleDateString("en-AU")
+                .replace("{name}", a.name)
+            )}`,
+          };
+          return sqs
             .sendMessage({
               QueueUrl: process.env.MAIL_QUEUE_URL,
-              MessageBody: JSON.stringify({
-                subject: `${title.replace("{name}", a.name)}`,
-                recipient: a.email,
-                body: `${description.replace(
-                  "{startDateTime}",
-                  new Date(startDateTime)
-                    .toLocaleDateString("en-AU")
-                    .replace("{name}", a.name)
-                )}`,
-              }),
+              MessageBody: JSON.stringify(params),
             })
-            .promise()
-        )
+            .promise();
+        })
       )
     : [];
 
@@ -79,7 +87,6 @@ export async function serviceMailer(service: Service, sendOverride = false) {
                   .publish({
                     PhoneNumber: a.phone,
                     Message: `${title.replace("{name}", a.name)}`,
-                    
                   })
                   .promise(),
               ]
@@ -87,10 +94,6 @@ export async function serviceMailer(service: Service, sendOverride = false) {
         )
       )
     : [];
-  console.log(notifyBookees);
-  console.log(notifyBookeesText);
-  console.log("Emails sent: " + notifyBookees.length);
-  console.log("Texts sent: " + notifyBookeesText.length);
-
+  console.log([...notifyBookeesText, ...notifyBookees]);
   return Promise.all([...notifyBookeesText, ...notifyBookees]);
 }
