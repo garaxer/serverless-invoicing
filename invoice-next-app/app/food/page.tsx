76b2683543ce397a8dd0f58e13576ac6407bf229
Gary Bagnall <gbagnall8@gmail.com>
2023-08-23 13:43:49 +1000

    mvoe suspense out

--- invoice-next-app/app/food/page.tsx
+++ invoice-next-app/app/food/page.tsx
@@ -8,7 +8,7 @@ import RQFoodList from "@/app/food/RQFoodList";
 import { revalidatePath } from "next/cache";
 import AddButton from "./AddButton";
 
-export default function Home() {
+export default async function Home() {
   const foodReq = fetch("http://localhost:3000/api/foods", {
     cache: "no-cache",
   }).then((res) => res.json());
@@ -44,15 +44,15 @@ export default function Home() {
           <main className="max-w-6xl mx-auto mt-5 text-3xl">
             <Suspense fallback={<div>Loading...</div>}>
               <FoodList />
-              <form action={addFood}>
-                <input
-                  type="text"
-                  name="food"
-                  className="border border-gray-300 rounded-lg py-4 px-4 text-base font-normal text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                />
-                <AddButton />
-              </form>
             </Suspense>
+            <form action={addFood}>
+              <input
+                type="text"
+                name="food"
+                className="border border-gray-300 rounded-lg py-4 px-4 text-base font-normal text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+              />
+              <AddButton />
+            </form>
           </main>
           <br />
           <RQFoodList />
